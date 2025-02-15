99a8eb02b892f8093d1183f8325fb867da0dd6cd
Neel Smith <dnsmith.neel@gmail.com>
2025-02-15 07:13:16 -0500

    Tests

--- test/test_navigation.jl
+++ test/test_navigation.jl
@@ -1,12 +1,5 @@
 @testset "Test tagging of pages across a vault" begin
     v = Vault(joinpath(pwd(), "data", "presidents-vault"))
-    
-    presidents = tagged(v, "#president")
-    @test length(presidents) == 2
-    expectedpages = [ "Abraham Lincoln", "George Washington"]
-    for name in presidents
-        @test name in expectedpages
-    end
 
     abetags = tags(v, "Abraham Lincoln")
     @test length(abetags) == 2
@@ -14,5 +7,21 @@
     for abetag in abetags
         @test abetag in expectedtags
     end
-    
+
+    presidents = tagged(v, "#president")
+    @test length(presidents) == 2
+    expectedpages = [ "Abraham Lincoln", "George Washington"]
+    for name in presidents
+        @test name in expectedpages
+    end
+
+    abeinlinks = linkedto(v, "Abraham Lincoln")
+    @test length(abeinlinks) == 3
+    expectedinlinks = [ "Abraham Lincoln 1860 census", "crossreferences",  "overview"]
+    for abeinlink in abeinlinks
+        @test abeinlink in expectedinlinks
+    end
+
+    abeoutlinks = linkson(v, "Abraham Lincoln")
+    @test abeoutlinks == ["Lincoln"]
 end
