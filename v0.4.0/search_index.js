54c11ec81df80585bcae54f84a2b716d02ae7966
Documenter.jl <documenter@juliadocs.github.io>
2025-02-19 13:47:24 +0000

    build based on 7c7fba5

---
+++ v0.4.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Obsidian.jl","page":"Home","title":"Obsidian.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation has moved to quarto:  https://neelsmith.quarto.pub/obsidian/.","category":"page"}]
+}
