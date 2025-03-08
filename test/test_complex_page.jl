9264c44a8bfe4020a75f54309895a7cfa0fcb24b
Neel Smith <dnsmith.neel@gmail.com>
2025-03-08 09:11:48 -0500

    Add unit test

--- test/test_complex_page.jl
+++ test/test_complex_page.jl
@@ -2,5 +2,13 @@
     testroot = pwd()
     v = Vault(joinpath(testroot, "data", "syrena"))
     syrena = "Syrena Adams"
-    allpagelinks = linkson(v, syrena)
+    allpagelinks = linkson(v, syrena) |> sort
+    expected = ["Adams",
+    "Elizabeth Stagg",
+    "Friend Adams",
+    "Panton, Adams Ferry Cemetery",
+    "Syrena Adams overview",
+    "White"]
+
+    @test allpagelinks == expected 
 end
